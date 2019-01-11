cacd82326e75692e362661b618552a76ec761220
Carl in 't Veld <carl@intveld.nl>
2019-01-11 08:52:07 +0100

    FanOutFanInExample added

---
+++ FunctionApp/FunctionApp/FanOutFanInExample.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp
+{
+    public class FanOutFanInExample
+    {
+        [FunctionName("FanOutFanInExample")]
+        public static async Task<List<string>> Run(
+            [OrchestrationTrigger] DurableOrchestrationContextBase context)
+        {
+            const int totalOfProcesses = 20;
+            var tasks = new Task<string>[totalOfProcesses];
+            for (int i = 0; i < totalOfProcesses; i++)
+            {
+                tasks[i] = context.CallSubOrchestratorAsync<string>("SubOrchestratorExample", $"Activity {i}");
+            }
+
+            await Task.WhenAll(tasks);
+
+            //var list = new List<String>();
+            return tasks.Select(t => t.Result).ToList();
+
+        }
+    }
+}
---
+++ FunctionApp/FunctionApp/SubOrchestratorExample.cs
@@ -0,0 +1,22 @@
+﻿using FunctionApp1;
+using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp
+{
+    public class SubOrchestratorExample
+    {
+        [FunctionName("SubOrchestratorExample")]
+        public static async Task<string> Run(
+            [OrchestrationTrigger] DurableOrchestrationContextBase context)
+        {
+            var input = context.GetInput<string>();
+            var result = await context.CallActivityAsync<string>("E1_SayHello", input);
+            await context.CallActivityAsync<string>("SendSignalRMessageActivity", new SignalRDTO { id = context.InstanceId, message = input, progress = 20 });
+            return result;
+        }
+    }
+}
