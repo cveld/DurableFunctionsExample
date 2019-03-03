ef2beadddee33b9bf85a5c5278d2a2fc31fc2c4d
Frank Noorloos <franknoorloos@hotmail.com>
2019-03-03 13:58:21 +0100

    added plaatjes

--- angular/src/app/fanout/fanout.component.ts
+++ angular/src/app/fanout/fanout.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { EasyAuthService, SignalrinfoService } from '../shared';
 import { ConfigurationService } from '../lib/Configuration';
+import {Observable, interval} from 'rxjs';
 import { Subscription } from 'rxjs';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 interface IOrchestratorResult {
   id: string;
@@ -21,7 +23,9 @@ export class FanoutComponent implements OnInit {
   baseurl: string;
   orchestratorResult: IOrchestratorResult;
   private subscriptions: Array<Subscription> = new Array<Subscription>();
-  messages: Array<string> = new Array<string>();
+  messages: Array<ImageContainer> = new Array<ImageContainer>();
+  imageIndex: number;
+
 
   constructor(
     private easyAuth: EasyAuthService,
@@ -30,11 +34,19 @@ export class FanoutComponent implements OnInit {
     private signalrinfoService: SignalrinfoService) { }
 
   ngOnInit() {
+    this.imageIndex = 0;
     this.baseurl = this.configuration.getValue('functionsApp');
     this.subscriptions.push(this.signalrinfoService.fanout$.subscribe(data => {
-      this.messages.push(`Incoming: ${data}`);
+      const container = new ImageContainer();
+      container.text = `Incoming: ${data}`;
+      container.url = `https://mandelbrotstorage.blob.core.windows.net/testing123/${data}.png`;
+      this.messages.push(container);
   }));
 
+    interval(1000).subscribe(x => {
+      this.nextFrame();
+      console.log('working');
+    });
   }
 
   startClicked() {
@@ -42,4 +54,20 @@ export class FanoutComponent implements OnInit {
       this.orchestratorResult = data;
     });
   }
+
+  nextFrame(): void {
+    if (this.messages.length <= 0) {
+      return;
+    }
+    if ( this.imageIndex + 1 >= this.messages.length - 1 ) {
+      this.imageIndex = 0;
+    }
+    this.imageIndex += 1;
+  }
+
+}
+
+  class ImageContainer {
+    public url: string;
+    public text: string;
 }
